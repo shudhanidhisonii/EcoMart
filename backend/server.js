16e825250cf58f912a7a59bfa9582def438f89d0
Shudha Nidhi <imshudhanidhisoni@gmail.com>
2026-06-02 09:23:14 +0530

    allow cors

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.json())
 app.use(cors({
     origin: [
         "http://localhost:5173",
+        "https://ecomartt-nm0ru07es-shudha-nidhi-sonis-projects.vercel.app/"
     ],
     credentials: true
 }))
